33dd76770e785414403f477c64978efb0080423a
Alisson Filipe <alifdas10@gmail.com>
2024-12-01 16:46:50 -0300

    classes: Produto & relatório

--- src/manuntecao/Relatorio.java
+++ src/manuntecao/Relatorio.java
@@ -1,5 +1,11 @@
 package src.manuntecao;
 
-public class Relatorio {
+import java.util.List;
 
+public class Relatorio {
+    public static void gerarRelatorio(List<Produto> produtosVendidos) {
+        for (Produto produto : produtosVendidos) {
+            System.out.println("Produto vendido: " + produto.getNome() + ", Preço: " + produto.getPreco());
+        }
+    }
 }
